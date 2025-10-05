337797f7d7789ae7b1a4331775f0b52f991ddf93
JeffersonParraAriza <jparra961023@gmail.com>
2025-10-04 22:19:52 -0500

    :gear: Update

--- src/main/java/com/example/restcoreografia/controller/LaboratorioController.java
+++ src/main/java/com/example/restcoreografia/controller/LaboratorioController.java
@@ -2,16 +2,18 @@ package com.example.restcoreografia.controller;
 
 import com.example.restcoreografia.model.Examen;
 import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
-@RequestMapping("/V1/api/laboratorio")
+@RequestMapping("/laboratorio")
 public class LaboratorioController {
 
     private Map<Long, List<Examen>> examenes = new HashMap<>();
-    private Long idCounter = 4L; // tenemos 3 mocks precargados
+    private Long idCounter = 4L;
 
     @PostMapping("/ordenes/{pacienteId}")
     public Examen registrarLaboratorio(@PathVariable Long pacienteId, @RequestBody Examen examen) {
@@ -21,9 +23,30 @@ public class LaboratorioController {
         return examen;
     }
 
-    @GetMapping("/resultados/{pacienteId}")
-    public List<Examen> listar(@PathVariable Long pacienteId) {
-        return examenes.getOrDefault(pacienteId, Collections.emptyList());
+    @PostMapping("/resultados/{pacienteId}/{examenId}")
+    public ResponseEntity<?> registrarResultadoExamen(
+            @PathVariable Long pacienteId,
+            @PathVariable Long examenId,
+            @RequestBody Examen examen) {
+
+        List<Examen> examenesPaciente = examenes.get(pacienteId);
+        Map<String, String> response = new HashMap<>();
+        if (examenesPaciente == null) {
+            response.put("mensaje", "No se encontraron exámenes para el paciente: " + pacienteId);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }
+
+        Optional<Examen> examenExistente = examenesPaciente.stream()
+                .filter(e -> e.getId().equals(examenId))
+                .findFirst();
+
+        if (examenExistente.isEmpty()) {
+            response.put("mensaje", "No se encontró el examen con ID: " + examenId);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        }
+
+        examenExistente.get().setResultado(examen.getResultado());
+        return ResponseEntity.ok(examenExistente.get());
     }
 
     @PostConstruct
